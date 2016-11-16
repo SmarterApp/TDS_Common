9fdcde3e0289a7907d2ce66b7d8f61e2cf7e0c2b
Todd Miller <millerts@users.noreply.github.com>
2016-11-16 09:04:47 -0800

    Feature/java7 compliance (#6)
    * Add handling of timestamp to the Joda time instant
    
    * Added a Joda to Java and vice versa converstion utility class

--- src/main/java/tds/common/data/mapping/ResultSetMapperUtility.java
+++ src/main/java/tds/common/data/mapping/ResultSetMapperUtility.java
@@ -11,12 +11,12 @@ public class ResultSetMapperUtility {
     /**
      * Map a {@link java.sql.Timestamp} to an {@link java.time.Instant}.
      *
-     * @param rs The {@link java.sql.ResultSet} being processed.
+     * @param rs          The {@link java.sql.ResultSet} being processed.
      * @param columnLabel The name of the column that should be mapped.
      * @return An {@link java.time.Instant} representation of the {@link java.sql.Timestamp} if one exists; otherwise null.
      * @throws java.sql.SQLException in the event an error occurs processing the {@code RecordSet}.
      */
-    public static Instant mapTimeStampToInstant(ResultSet rs, String columnLabel) throws SQLException {
+    public static Instant mapTimestampToInstant(ResultSet rs, String columnLabel) throws SQLException {
         Timestamp ts = rs.getTimestamp(columnLabel);
         return ts == null
             ? null
@@ -35,4 +35,29 @@ public class ResultSetMapperUtility {
             : Timestamp.valueOf(LocalDateTime.ofInstant(time, ZoneOffset.UTC));
     }
 
+    /**
+     * Map a {@link org.joda.time.Instant} to a {@link java.sql.Timestamp}
+     *
+     * @param time the {@link org.joda.time.Instant} to convert
+     * @return the converted instant
+     */
+    public static Timestamp mapJodaInstantToTimestamp(org.joda.time.Instant time) {
+        if (time == null) return null;
+        Timestamp ts = new Timestamp(time.getMillis());
+        return ts;
+    }
+
+    /**
+     * Map a {@link java.sql.Timestamp} to a {@link org.joda.time.Instant}
+     *
+     * @param rs          {@link java.sql.ResultSet} containing the data to convert
+     * @param columnLabel the column label used to fetch the timestamp from the result set
+     * @return converted {@link org.joda.time.Instant}
+     * @throws SQLException in the event an error occurs accessing the result set
+     */
+    public static org.joda.time.Instant mapTimestampToJodaInstant(ResultSet rs, String columnLabel) throws SQLException {
+        Timestamp ts = rs.getTimestamp(columnLabel);
+        return ts == null ? null :
+            new org.joda.time.Instant(ts.getTime());
+    }
 }
