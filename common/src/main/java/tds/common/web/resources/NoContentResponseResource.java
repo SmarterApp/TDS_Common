8331762d70496dc2d80dfb0b3ac3dc25c0f440b7
Jeff Johnson <jeffjohnson9046@hotmail.com>
2016-12-26 17:38:07 -0800

    Feature/pause exam (#11)
    * TDS-341: Added NoContentResponseResource
    * TDS-341: Removed optional from getErrors()
    * TDS-341: updated pom.xml

---
+++ common/src/main/java/tds/common/web/resources/NoContentResponseResource.java
@@ -0,0 +1,24 @@
+package tds.common.web.resources;
+
+import tds.common.ValidationError;
+
+/**
+ * Contains errors (if any) when responding without data (e.g. when the response would be a 204 - No Content).
+ */
+public class NoContentResponseResource {
+    private ValidationError[] errors;
+
+    /**
+     * @param errors errors present when completing the request
+     */
+    public NoContentResponseResource(ValidationError... errors) {
+        this.errors = errors;
+    }
+
+    /**
+     * @return errors when completing the request.
+     */
+    public ValidationError[] getErrors() {
+        return errors;
+    }
+}
