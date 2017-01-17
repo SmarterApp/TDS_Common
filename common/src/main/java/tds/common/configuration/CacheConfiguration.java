8b8482f4c637643977db0efe65d5fbc54dccdaca
Ernie Munoz <emunoz@fairwaytech.com>
2017-01-16 18:22:05 -0800

    Making cache expiration time external properties

--- common/src/main/java/tds/common/configuration/CacheConfiguration.java
+++ common/src/main/java/tds/common/configuration/CacheConfiguration.java
@@ -2,6 +2,7 @@ package tds.common.configuration;
 
 import com.google.common.cache.CacheBuilder;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
@@ -9,6 +10,7 @@ import org.springframework.cache.guava.GuavaCache;
 import org.springframework.cache.guava.GuavaCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 import java.util.concurrent.TimeUnit;
 
@@ -17,7 +19,18 @@ import java.util.concurrent.TimeUnit;
  */
 @Configuration
 @EnableCaching
+@PropertySource("classpath:cache.properties")
 public class CacheConfiguration {
+
+    @Value("${tds.cache.expire.time.short:20}")
+    private Integer shortTermExpirationInSeconds;
+
+    @Value("${tds.cache.expire.time.medium:600}")
+    private Integer mediumTermExpirationInSeconds;
+
+    @Value("${tds.cache.expire.time.long:7200}")
+    private Integer longTermExpirationInSeconds;
+
     @Bean
     public CacheManager cacheManager() {
         return new GuavaCacheManager(CacheType.SHORT_TERM, CacheType.MEDIUM_TERM, CacheType.LONG_TERM);
@@ -27,7 +40,7 @@ public class CacheConfiguration {
     @Qualifier(CacheType.SHORT_TERM)
     public Cache shortTermCache() {
         return new GuavaCache(CacheType.SHORT_TERM, CacheBuilder.newBuilder()
-            .expireAfterWrite(20, TimeUnit.SECONDS)
+            .expireAfterWrite(shortTermExpirationInSeconds, TimeUnit.SECONDS)
             .build());
     }
 
@@ -35,7 +48,7 @@ public class CacheConfiguration {
     @Qualifier(CacheType.MEDIUM_TERM)
     public Cache mediumTermCache() {
         return new GuavaCache(CacheType.MEDIUM_TERM, CacheBuilder.newBuilder()
-            .expireAfterWrite(600, TimeUnit.SECONDS)
+            .expireAfterWrite(mediumTermExpirationInSeconds, TimeUnit.SECONDS)
             .build());
     }
 
@@ -43,7 +56,7 @@ public class CacheConfiguration {
     @Qualifier(CacheType.LONG_TERM)
     public Cache longTermCache() {
         return new GuavaCache(CacheType.LONG_TERM, CacheBuilder.newBuilder()
-            .expireAfterWrite(7200, TimeUnit.SECONDS)
+            .expireAfterWrite(longTermExpirationInSeconds, TimeUnit.SECONDS)
             .build());
     }
 }
