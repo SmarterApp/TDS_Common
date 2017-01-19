0efa323c5a1a153556a72c268060de06ec827b9b
Todd Miller <tmiller@fairwaytech.com>
2017-01-18 16:05:38 -0800

    Added a translation message to the validation error so the services can return translated messages to be displayed on screen.

--- legacy/src/main/java/tds/common/ValidationError.java
+++ legacy/src/main/java/tds/common/ValidationError.java
@@ -1,5 +1,7 @@
 package tds.common;
 
+import com.google.common.base.Optional;
+
 import static tds.common.util.Preconditions.checkNotNull;
 
 /**
@@ -8,14 +10,25 @@ import static tds.common.util.Preconditions.checkNotNull;
 public class ValidationError {
     private final String code;
     private final String message;
+    private final String translatedMessage;
 
     /**
      * @param code    error code for the error type
      * @param message error message to describe why error occurred
      */
     public ValidationError(String code, String message) {
+        this(code, message, null);
+    }
+
+    /**
+     * @param code              error code for the error type
+     * @param message           error message to describe why error occurred
+     * @param translatedMessage the translated error message
+     */
+    public ValidationError(String code, String message, String translatedMessage) {
         this.code = checkNotNull(code);
-        this.message = message;
+        this.message = checkNotNull(message);
+        this.translatedMessage = translatedMessage;
     }
 
     /**
@@ -30,5 +43,12 @@ public class ValidationError {
      */
     public String getMessage() {
         return message;
+    }
+
+    /**
+     * @return optional containing the
+     */
+    public Optional<String> getTranslatedMessage() {
+        return Optional.fromNullable(translatedMessage);
     }
 }
