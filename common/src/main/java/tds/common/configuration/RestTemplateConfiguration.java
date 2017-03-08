e1a83b9a0119ff32d967ff8847174caee31d6141
Ernie Munoz <emunoz@fairwaytech.com>
2017-03-08 11:08:12 -0800

    Upgrading spring boot version, using RestTemplateBuilder

--- common/src/main/java/tds/common/configuration/RestTemplateConfiguration.java
+++ common/src/main/java/tds/common/configuration/RestTemplateConfiguration.java
@@ -1,7 +1,8 @@
 package tds.common.configuration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -9,21 +10,18 @@ import org.springframework.http.client.BufferingClientHttpRequestFactory;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Arrays;
-
 import tds.common.web.interceptors.RestTemplateLoggingInterceptor;
 
 @Configuration
 @Import(JacksonObjectMapperConfiguration.class)
 public class RestTemplateConfiguration {
 
-    @Autowired
-    private ObjectMapper objectMapper;
-
     @Bean
-    public RestTemplate restTemplate() {
-        RestTemplate restTemplate = new RestTemplate(new BufferingClientHttpRequestFactory(new SimpleClientHttpRequestFactory()));
-        restTemplate.setInterceptors(Arrays.asList(new RestTemplateLoggingInterceptor(objectMapper)));
-        return restTemplate;
+    @ConditionalOnMissingBean(RestTemplate.class)
+    public RestTemplate restTemplate(final RestTemplateBuilder builder, final ObjectMapper objectMapper) {
+        return builder
+            .requestFactory(new BufferingClientHttpRequestFactory(new SimpleClientHttpRequestFactory()))
+            .additionalInterceptors(new RestTemplateLoggingInterceptor(objectMapper))
+            .build();
     }
 }
