feba8f6d06da1f055441f5ed1dec5427014feb94
Ernie Munoz <emunoz@fairwaytech.com>
2017-03-08 11:56:41 -0800

    Updates based on PR feedback - minor refactoring, adding traceId

--- common/src/main/java/tds/common/web/interceptors/RestTemplateLoggingInterceptor.java
+++ common/src/main/java/tds/common/web/interceptors/RestTemplateLoggingInterceptor.java
@@ -2,6 +2,7 @@ package tds.common.web.interceptors;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.base.Charsets;
+import com.google.common.io.CharStreams;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
@@ -11,9 +12,10 @@ import org.springframework.http.client.ClientHttpRequestExecution;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.http.client.ClientHttpResponse;
 
-import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.UUID;
 
 /**
  * An interceptor for logging REST requests and responses
@@ -21,25 +23,28 @@ import java.io.InputStreamReader;
 public class RestTemplateLoggingInterceptor implements ClientHttpRequestInterceptor {
     private static final Logger log = LoggerFactory.getLogger(RestTemplateLoggingInterceptor.class);
     private static final String CONTENT_TYPE_SUBTYPE_JSON = "json";
-    private ObjectMapper objectMapper;
+    private final ObjectMapper objectMapper;
 
-    public RestTemplateLoggingInterceptor(ObjectMapper objectMapper) {
+    public RestTemplateLoggingInterceptor(final ObjectMapper objectMapper) {
         this.objectMapper = objectMapper;
     }
 
     @Override
     public ClientHttpResponse intercept(final HttpRequest request, final byte[] body, final ClientHttpRequestExecution clientHttpRequestExecution) throws IOException {
-        logRequest(request, body);
+        final UUID traceId = UUID.randomUUID();
+        logRequest(request, body, traceId);
         ClientHttpResponse response = clientHttpRequestExecution.execute(request, body);
-        logResponse(response);
+        logResponse(response, traceId);
         return response;
     }
 
-    private void logRequest(final HttpRequest request, final byte[] body) {
+    private void logRequest(final HttpRequest request, final byte[] body, final UUID traceId) {
         final HttpHeaders headers = request.getHeaders();
         String bodyString = new String(body, Charsets.UTF_8);
 
-        if (!bodyString.isEmpty() && headers.getAccept().contains(MediaType.APPLICATION_JSON)) {
+        MediaType contentType = request.getHeaders().getContentType();
+
+        if (!bodyString.isEmpty() && contentType.getSubtype().equals(CONTENT_TYPE_SUBTYPE_JSON)) {
             try {
                 final Object json = objectMapper.readValue(bodyString, Object.class);
                 bodyString = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(json);
@@ -49,24 +54,20 @@ public class RestTemplateLoggingInterceptor implements ClientHttpRequestIntercep
         }
 
         log.debug("=========================================* REQUEST *==========================================");
+        log.debug(" Trace ID    :   {}", traceId);
         log.debug(" Method/URI  :   {} - {}", request.getMethod(), request.getURI());
         log.debug(" Headers     :   {} ", request.getHeaders());
         log.debug(" Body        :   {} ", bodyString);
         log.debug("==============================================================================================");
     }
 
-    private void logResponse(final ClientHttpResponse response) throws IOException {
-        StringBuilder inputStringBuilder = new StringBuilder();
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(response.getBody(), Charsets.UTF_8));
-        String line = bufferedReader.readLine();
+    private void logResponse(final ClientHttpResponse response, final UUID traceId) throws IOException {
+        String bodyString;
 
-        while (line != null) {
-            inputStringBuilder.append(line);
-            inputStringBuilder.append('\n');
-            line = bufferedReader.readLine();
+        try (final InputStream in = response.getBody()) {
+            bodyString = CharStreams.toString(new InputStreamReader(in, Charsets.UTF_8));
         }
 
-        String bodyString = inputStringBuilder.toString();
         MediaType contentType = response.getHeaders().getContentType();
 
         if (!bodyString.isEmpty() && contentType.getSubtype().equals(CONTENT_TYPE_SUBTYPE_JSON)) {
@@ -79,6 +80,7 @@ public class RestTemplateLoggingInterceptor implements ClientHttpRequestIntercep
         }
 
         log.debug("=========================================* RESPONSE *=========================================");
+        log.debug(" Trace ID    :   {}", traceId);
         log.debug(" Status Code :   {}", response.getStatusCode());
         log.debug(" Status Text :   {}", response.getStatusText());
         log.debug(" Headers     :   {}", response.getHeaders());
