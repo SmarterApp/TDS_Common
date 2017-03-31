34fc6d216df75f92b11e6c377936334902d10ba0
Jeffrey C Jones <jjones@fairwaytech.com>
2017-03-31 00:45:23 -0700

    Simplify timing code, force EventLogger to be request scoped.

--- common/src/main/java/tds/common/logging/EventLogger.java
+++ common/src/main/java/tds/common/logging/EventLogger.java
@@ -34,6 +34,9 @@ public class EventLogger {
         HTTP_REQUEST_PARAMETERS,
         HTTP_REQUEST_UUID,
         HTTP_SESSION_ID,
+        MILLIS_CURRENT,
+        MILLIS_END,
+        MILLIS_BEGIN,
         RESPONSE_CODE,
         RESPONSE_CODE_TEXT,
         RESULT,
@@ -44,8 +47,9 @@ public class EventLogger {
     private static final Logger logger = LoggerFactory.getLogger(EventLogger.class);
     private static final Marker marker = MarkerFactory.getMarker("event-logger");
     private final ObjectWriter writer;
-    private final Map<String, Long> eventTimes = new HashMap<>();
     private final Map<String, Object> extraFields = new HashMap<>();
+    private long beginMillis = 0;
+    private long endMillis = 0;
 
     public EventLogger(final ObjectMapper objectMapper) {
         this.writer = objectMapper.writer();
@@ -79,7 +83,7 @@ public class EventLogger {
      */
     public void info(final String app, final String logEvent, final String checkpoint,
                      final String message, final Map<EventData, Object> data) {
-        log(formatMessage(logEvent, message), getFieldMap(app, logEvent, checkpoint, data));
+        log(formatMessage(logEvent, message), getFieldMap(app, checkpoint, data));
     }
 
     /**
@@ -95,7 +99,15 @@ public class EventLogger {
      */
     public void error(final String app, final String logEvent, final String checkpoint,
                       String message, final Map<EventData, Object> data, final Exception e) {
-        error(formatMessage(logEvent, message), getFieldMap(app, logEvent, checkpoint, data), e);
+        error(formatMessage(logEvent, message), getFieldMap(app, checkpoint, data), e);
+    }
+
+    public void setBeginMillis(long beginMillis) {
+        this.beginMillis = beginMillis;
+    }
+
+    public void setEndMillis(long endMillis) {
+        this.endMillis = endMillis;
     }
 
     private void log(final String event, final Map<String, Object> fields) {
@@ -115,25 +127,18 @@ public class EventLogger {
         }
     }
 
-    private void addTimerMetrics(final String event, final String checkpoint, Map<EventData, Object> fields) {
-        Long eventTime = eventTimes.get(event);
-        if (Checkpoint.ENTER.name().equals(checkpoint)) {
-            // entry point - (re)set eventTime and emit nothing
-            eventTimes.put(event, System.currentTimeMillis());
-        } else {
-            if (Checkpoint.EXIT.name().equals(checkpoint)) {
-                // exit point - remove timestamp. falls through to default to emit metric.
-                eventTimes.remove(event);
-            }
-            // non entry/exit or missing position marker - emit elapsed metric
-            if (null != eventTime) {
-                fields.put(EventData.ELAPSED_TIME, System.currentTimeMillis() - eventTime);
-            }
+    private void addTimerMetrics(Map<EventData, Object> fields) {
+        fields.put(EventData.MILLIS_CURRENT, System.currentTimeMillis());
+        if(beginMillis != 0) {
+            fields.put(EventData.MILLIS_BEGIN, beginMillis);
+            fields.put(EventData.ELAPSED_TIME, System.currentTimeMillis() - beginMillis);
+        }
+        if(endMillis != 0) {
+            fields.put(EventData.MILLIS_END, endMillis);
         }
     }
 
-    private Map<String, Object> getFieldMap(final String app, final String event, String checkpoint,
-                                            final Map<EventData, Object> data) {
+    private Map<String, Object> getFieldMap(final String app, String checkpoint, final Map<EventData, Object> data) {
         Map<EventData, Object> fields = new HashMap<>();
         if (!StringUtils.isEmpty(app)) {
             fields.put(EventData.APP, app);
@@ -144,7 +149,7 @@ public class EventLogger {
         if (null != data) {
             fields.putAll(data);
         }
-        addTimerMetrics(event, checkpoint, fields);
+        addTimerMetrics(fields);
 
         // Write all data into outputMap, lowercasing key names.
         Map<String, Object> outputMap = new HashMap<>();
--- common/src/main/java/tds/common/web/interceptors/RestTemplateEventLoggingInterceptor.java
+++ common/src/main/java/tds/common/web/interceptors/RestTemplateEventLoggingInterceptor.java
@@ -7,14 +7,12 @@ import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.http.client.ClientHttpResponse;
 
 import java.io.IOException;
-import java.util.UUID;
 
 import tds.common.logging.EventLogger;
 
 import static tds.common.logging.EventLogger.Checkpoint.SERVICE_CALL;
 import static tds.common.logging.EventLogger.Checkpoint.SERVICE_RETURN;
 import static tds.common.logging.EventLogger.EventData.HTTP_HEADERS;
-import static tds.common.logging.EventLogger.EventData.HTTP_REQUEST_UUID;
 import static tds.common.logging.EventLogger.EventData.RESPONSE_CODE;
 import static tds.common.logging.EventLogger.EventData.RESPONSE_CODE_TEXT;
 
@@ -23,36 +21,38 @@ import static tds.common.logging.EventLogger.EventData.RESPONSE_CODE_TEXT;
  */
 public class RestTemplateEventLoggingInterceptor implements ClientHttpRequestInterceptor {
 
+    private ObjectMapper objectMapper;
     private String appId;
-    private EventLogger eventLogger;
 
     public RestTemplateEventLoggingInterceptor(final ObjectMapper objectMapper, String appId) {
+        this.objectMapper = objectMapper;
         this.appId = appId;
-        eventLogger = new EventLogger(objectMapper);
-        eventLogger.putField(HTTP_REQUEST_UUID.name(), UUID.randomUUID());
     }
 
     @Override
     public ClientHttpResponse intercept(final HttpRequest request,
                                         final byte[] body,
                                         final ClientHttpRequestExecution clientHttpRequestExecution) throws IOException {
-        logRequest(request);
+        EventLogger eventLogger = new EventLogger(objectMapper);
+        logRequest(eventLogger, request);
         ClientHttpResponse response = clientHttpRequestExecution.execute(request, body);
-        logResponse(request, response);
+        logResponse(eventLogger, request, response);
         return response;
     }
 
-    private void logRequest(HttpRequest request) {
+    private void logRequest(EventLogger eventLogger, HttpRequest request) {
         eventLogger.putField(HTTP_HEADERS.name(), request.getHeaders());
+        eventLogger.setBeginMillis(System.currentTimeMillis());
         eventLogger.info(appId, request.getURI().getPath(), SERVICE_CALL.name(), null, null);
     }
 
-    private void logResponse(HttpRequest request, ClientHttpResponse response) {
+    private void logResponse(EventLogger eventLogger, HttpRequest request, ClientHttpResponse response) {
         try {
             eventLogger.putField(RESPONSE_CODE.name(), response.getStatusCode());
             eventLogger.putField(RESPONSE_CODE_TEXT.name(), response.getStatusText());
         } catch (Exception ignored) {
         }
+        eventLogger.setEndMillis(System.currentTimeMillis());
         eventLogger.info(appId, request.getURI().getPath(), SERVICE_RETURN.name(), null, null);
     }
 }
