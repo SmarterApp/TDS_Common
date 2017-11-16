f898d9d4c2bbea98aaa5b152dff7d82569dfa136
Todd Miller <tmiller@fairwaytech.com>
2017-11-15 17:17:07 -0800

    Modify the change listener to accept EntityUpdate rather than old and new types which allows for list of updates

--- common/src/main/java/tds/common/entity/utils/ChangeListener.java
+++ common/src/main/java/tds/common/entity/utils/ChangeListener.java
@@ -13,6 +13,8 @@
 
 package tds.common.entity.utils;
 
+import tds.common.EntityUpdate;
+
 /**
  * An interface for listening for and responding to changes on an entity
  */
@@ -21,8 +23,6 @@ public interface ChangeListener<T> {
      * Compare an original instance of an object's state to an updated instance of an object's state and execute any
      * business rules/logic that are a consequence of the change in state.
      *
-     * @param oldInstance The original instance of the object/entity
-     * @param newInstance The updated instance of the object/entity
      */
-    void accept(T oldInstance, T newInstance);
+    void accept(EntityUpdate<T> entityUpdate);
 }
