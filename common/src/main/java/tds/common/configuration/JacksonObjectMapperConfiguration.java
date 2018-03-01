e839709ee569d625aa2b645118c70316441e73ee
Ernie Munoz <emunoz@fairwaytech.com>
2018-03-01 14:26:02 -0800

    Adding JDK8 modules to object mapper

--- common/src/main/java/tds/common/configuration/JacksonObjectMapperConfiguration.java
+++ common/src/main/java/tds/common/configuration/JacksonObjectMapperConfiguration.java
@@ -16,7 +16,9 @@ package tds.common.configuration;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.guava.GuavaModule;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.joda.JodaModule;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -27,9 +29,9 @@ public class JacksonObjectMapperConfiguration {
     public Jackson2ObjectMapperBuilderCustomizer customizeObjectMapper() {
         return builder -> {
             builder.featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-            builder.serializationInclusion(JsonInclude.Include.NON_NULL);
+            builder.serializationInclusion(JsonInclude.Include.NON_EMPTY);
             builder.failOnUnknownProperties(false);
-            builder.modules(new GuavaModule(), new JodaModule());
+            builder.modules(new GuavaModule(), new JodaModule(), new Jdk8Module(), new JavaTimeModule());
         };
     }
 }
